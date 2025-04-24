8909526b51156cc1df737fee8cd8b5091ba88998
manaswini79 <manaswini079@gmail.com>
2025-04-24 21:05:51 +0530

    Create semi_structured_query.sql

---
+++ 2025-04-24_DSU/semi_structured_query.sql
@@ -0,0 +1,19 @@
+
+-- Query 3: Check if 'interests' Field Exists:  
+-- > For each customer, check if the `interests` field exists in the `profile`
+-- JSON. Return the customer `name` and a new column called `has_interests`, 
+-- which should be `'Yes'` if `interests` exists and `'No'` if it does not.
+
+-- Sample Output::
+
+-- NAME	AGE	LOCATION
+-- Alice	30	"New York"
+-- Bob	25	"San Francisco"
+-- Charlie	35	"Chicago"
+
+
+<config>
+    <url value="https://app.snowflake.com/xxxxx/xxxxxx/"></url>
+    <username value=""></username>
+    <password value=""></password>
+</config>
