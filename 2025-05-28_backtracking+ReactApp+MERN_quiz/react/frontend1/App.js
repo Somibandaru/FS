270a00c5bbfb56ef30e287af017d7d4f537c02ff
manaswini79 <manaswini079@gmail.com>
2025-05-28 14:01:41 +0530

    Create App.js

---
+++ 2025-05-28_backtracking+ReactApp+MERN_quiz/react/frontend1/App.js
@@ -0,0 +1,12 @@
+import Product from './Product';
+
+const App =() =>{
+   return(
+      <div style={{ padding: '2rem' }}>
+          <h2>Product Catalog</h2>
+         <Product />
+      </div>
+   )
+   
+}
+export default App;
