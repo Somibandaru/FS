97fae60b3176b55613141fc8e19c2ef5857c472b
Ruchira1214 <155610185+Ruchira1214@users.noreply.github.com>
2025-06-10 13:59:19 +0530

    Update dp1.java

--- 2025-06-10_Dp_Python_SQL/dp1.java
+++ 2025-06-10_Dp_Python_SQL/dp1.java
@@ -76,67 +76,57 @@
 
 import java.util.*;
 
-public class LCPS {
-    public static void main(String[] args) {
+class test {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String s1 = sc.next();
         String s2 = sc.next();
 
-        String lcs = getLCS(s1, s2);
-        int result = getLPSLength(lcs);
-
-        System.out.println(result);
-    }
-
-    // Step 1: Longest Common Subsequence
-    private static String getLCS(String s1, String s2) {
-        int n = s1.length(), m = s2.length();
-        int[][] dp = new int[n + 1][m + 1];
-
-        // Build the LCS dp table
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1))
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+        // Step 1: LCS DP table
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            for (int j = s2.length() - 1; j >= 0; j--) {
+                if (s1.charAt(i) == s2.charAt(j))
+                    dp[i][j] = dp[i + 1][j + 1] + 1;
                 else
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j]);
             }
         }
 
-        // Reconstruct the LCS string
-        StringBuilder lcs = new StringBuilder();
-        int i = n, j = m;
-        while (i > 0 && j > 0) {
-            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                lcs.append(s1.charAt(i - 1));
-                i--;
-                j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1])
-                i--;
+        // Step 2: Reconstruct LCS
+        StringBuilder sb = new StringBuilder();
+        int i = 0, j = 0;
+        while (i < s1.length() && j < s2.length()) {
+            if (s1.charAt(i) == s2.charAt(j)) {
+                sb.append(s1.charAt(i));
+                i++;
+                j++;
+            } else if (dp[i + 1][j] >= dp[i][j + 1])
+                i++;
             else
-                j--;
+                j++;
         }
 
-        return lcs.reverse().toString(); // LCS is built backwards
+        String lcs = sb.toString();
+        String lcsR = rev(lcs);
+
+        // Step 3: LPS from LCS by LCS(lcs, reverse(lcs))
+        int[][] ldp = new int[lcs.length() + 1][lcs.length() + 1];
+        for (int l = lcs.length() - 1; l >= 0; l--) {
+            for (int k = lcs.length() - 1; k >= 0; k--) {
+                if (lcs.charAt(l) == lcsR.charAt(k))
+                    ldp[l][k] = ldp[l + 1][k + 1] + 1;
+                else
+                    ldp[l][k] = Math.max(ldp[l][k + 1], ldp[l + 1][k]);
+            }
+        }
+
+        // Step 4: Output the LPS length from LCS
+        System.out.println(ldp[0][0]);
     }
 
-    // Step 2: Longest Palindromic Subsequence
-    private static int getLPSLength(String s) {
-        int n = s.length();
-        int[][] dp = new int[n][n];
-
-        for (int len = 1; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1;
-                if (i == j) dp[i][j] = 1;
-                else if (s.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = 2 + ((i + 1 <= j - 1) ? dp[i + 1][j - 1] : 0);
-                } else {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-
-        return n == 0 ? 0 : dp[0][n - 1];
+    public static String rev(String s) {
+        StringBuilder sb = new StringBuilder(s); // Fix: pass the string
+        return sb.reverse().toString();
     }
 }
