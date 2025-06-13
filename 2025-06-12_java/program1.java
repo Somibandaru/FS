386a7747d5cc0747b4d3f6a9827f38ac68799b8d
Ruchira1214 <155610185+Ruchira1214@users.noreply.github.com>
2025-06-13 15:30:07 +0530

    Create program1.java

---
+++ 2025-06-12_java/program1.java
@@ -0,0 +1,61 @@
+/*Luke likes to construct and play with arrays. His dad gave him an array M of 
+length N and assigned him the following tasks to be done in order:
+ - reate continuous groups of size i from the array M (where i goes from 1 to N).
+ - For each group of size i, find the minimum value.
+ - Among all the minimums from that step, select the maximum.
+ - Add this selected value as the i-th element of a new array P.
+ - Repeat until i = N.
+
+Note: Use 1-based indexing for array P in logic.
+
+Input Format:
+-------------
+input1: Integer N – length of array M
+input2: Integer array M of length N
+
+Output Format:
+--------------
+Return the array P as output.
+
+Sample Input:
+-------------
+3
+1 2 3
+
+Sample Output:
+--------------
+3 2 1
+
+
+Sample Input: 
+-------------
+4
+20 10 30 40
+
+Sample Output: 
+--------------
+40 30 10 10
+*/
+import java.util.*;
+class test{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i=0;i<n;i++) a[i] = sc.nextInt();
+        int[] res = new int[n];
+        int w = 1;
+        for(int i=1;i<=n;i++){
+            int max = 0;
+            for(int j=0;j<=n-i;j++){
+                int min = Integer.MAX_VALUE;
+                for(int k=0;k<i;k++){
+                    min = Math.min(min,a[k+j]);
+                }
+                max = Math.max(max,min);
+            }
+            res[i-1] = max;
+        }
+        for(int x : res) System.out.print(x + " ");
+    }
+}
